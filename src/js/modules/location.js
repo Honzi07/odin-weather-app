135c35f96820d08caf9215ec33ef461b50394e65
Honzi07 <nyisztor.zsolt@gmail.com>
2024-09-10 21:02:02 +0200

    Move DOM-related code to its own module.

--- src/js/modules/location.js
+++ src/js/modules/location.js
@@ -1,8 +1,6 @@
-const form = document.querySelector('form');
-
 function getLocation() {
   const inputSearch = document.querySelector('#search');
   return inputSearch.value;
 }
 
-export { getLocation, form };
+export { getLocation };
