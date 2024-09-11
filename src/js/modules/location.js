8d9f71b956230a266afd623ee1ffb3cb19a35719
Honzi07 <nyisztor.zsolt@gmail.com>
2024-09-11 06:45:05 +0200

    Add geolocation for automatic user location retrieval and convert function declarations to expressions.

--- src/js/modules/location.js
+++ src/js/modules/location.js
@@ -1,6 +1,36 @@
-function getLocation() {
+const getLocationInput = () => {
   const inputSearch = document.querySelector('#search');
-  return inputSearch.value;
-}
+  console.log({ loc: inputSearch.value });
+  // return inputSearch.value;
+  return { loc: inputSearch.value };
+};
 
-export { getLocation };
+const locationData = (pos) => {
+  console.log(pos);
+
+  const position = {
+    lat: pos.coords.latitude,
+    lon: pos.coords.longitude,
+  };
+  console.log(position);
+
+  // getWeather2(position.latitude, position.longitude);
+
+  // return {
+  //   lat: pos.coords.latitude,
+  //   lon: pos.coords.longitude,
+  // };
+};
+
+const error = (err) => {
+  console.error(`ERROR(${err.code}): ${err.message}`);
+};
+
+const locationOptions = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+};
+
+navigator.geolocation.getCurrentPosition(locationData, error, locationOptions);
+
+export { getLocationInput };
