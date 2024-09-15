f46c02dd8e26216d13ee05afdadcffc86236ae60
Honzi07 <nyisztor.zsolt@gmail.com>
2024-09-15 04:19:27 +0200

    Add function to combine data and return it for others.

--- src/js/modules/dom.js
+++ src/js/modules/dom.js
@@ -5,8 +5,26 @@ function getHtmlElement() {
     form: document.querySelector('.search-form'),
     searchInput: document.querySelector('.search-input'),
     btnGeo: document.querySelector('.geolocation-btn'),
+    status: document.querySelector('.status-message'),
   };
 }
 
-// const { form } = getHtmlElement();
-const form = getHtmlElement().form;
+getHtmlElement().form.addEventListener('submit', showWeather);
+
+getHtmlElement().btnGeo.addEventListener('click', showWeather);
+
+async function handleWeatherEv(ev) {
+  ev.preventDefault();
+
+  if (ev.target.classList.contains('geolocation-btn')) {
+    return await getProcessGeoWeather();
+  } else if (ev.target.classList.contains('search-form')) {
+    return await getProcessUserWeather();
+  }
+}
+
+async function showWeather(ev) {
+  const data = await handleWeatherEv(ev);
+  console.log('test', data);
+  getHtmlElement().status.textContent = data.description;
+}
