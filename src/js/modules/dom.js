21ba9a6f4737bbe8044cb8c566afdbb2c297869a
Honzi07 <nyisztor.zsolt@gmail.com>
2024-09-30 07:45:33 +0200

    Add dynamic weather display and alert functionality
    Add current weather display based on users measurement choice and location. Implement weather alerts display. Add function to render current elements with future scalability.

--- src/js/modules/dom.js
+++ src/js/modules/dom.js
@@ -2,33 +2,113 @@ import { getProcessGeoWeather, getProcessUserWeather } from './data';
 
 function getHtmlElement() {
   return {
-    form: document.querySelector('.search-form'),
-    searchInput: document.querySelector('.search-input'),
-    btnGeo: document.querySelector('.geolocation-btn'),
-    status: document.querySelector('.status-message'),
+    form: document.querySelector('.search-container__form'),
+    searchInput: document.querySelector('.search-container__input'),
+    btnGeo: document.querySelector('.search-container__geolocation-btn'),
+    summary: document.querySelector('.current-weather__summary'),
   };
 }
-
-getHtmlElement().form.addEventListener('submit', showWeather);
-
-getHtmlElement().btnGeo.addEventListener('click', showWeather);
 
 async function handleWeatherEv(ev) {
   ev.preventDefault();
 
-  if (ev.target.classList.contains('geolocation-btn')) {
+  if (ev.target.classList.contains('search-container__geolocation-btn')) {
     return await getProcessGeoWeather();
-  } else if (ev.target.classList.contains('search-form')) {
+  } else if (ev.target.classList.contains('search-container__form')) {
     return await getProcessUserWeather();
   }
 }
 
-function getWeatherIcon(icon) {
-  const svg = require('../../assets/images/' + icon + '.svg');
+const getWeatherIcon = (icon) =>
+  require('../../assets/images/' + icon + '.svg');
 
-  return svg;
+const displayAlerts = (data) => {
+  if (!data.alerts.length) return;
+  console.log('alert');
+
+  const container = document.querySelector('.current-weather__alert-container');
+
+  for (const alert of data.alerts) {
+    console.log(alert);
+    const html = `<div class="current-weather__alert-description">${alert.description}  <a href="${alert.link}"  target="_blank" rel="noopener noreferrer">More Information</a></div>`;
+
+    container.insertAdjacentHTML('afterbegin', html);
+  }
+};
+
+async function currentWeatherElement(data, element) {
+  const html = `
+        <div class="current-weather">
+        <header class="current-weather__header">
+          <div class="current-weather__today-label">Weather currently</div>
+          <p class="current-weather__update-time">${data.currently.datetime}</p>
+        </header>
+
+        <div class="current-weather__summary-container">
+          <div class="current-weather__alert-container">
+          </div>
+          <div class="current-weather__overview-current-temp">
+            <img
+              class="current-weather__image"
+              src="${getWeatherIcon(data.currently.icon)}"
+              alt="${data.currently.icon} weather icon"
+            />
+            <div class="current-weather__temp">${Math.round(
+              data.currently.temp
+            )}</div>
+            <div class="current-weather__condition-details">
+              <div class="current-weather__condition">${
+                data.currently.conditions
+              }</div>
+              <div class="current-weather__feelslike-container">
+                <div class="current-weather__feelslike-label">Feels like</div>
+                <span class="current-weather__feelslike-temp">${Math.round(
+                  data.currently.feelslike
+                )}</span>
+              </div>
+            </div>
+          </div>
+          <div class="current-weather__summary">
+          ${data.description}
+          </div>
+        </div>
+        <div class="current-weather__detail-container">
+          <div class="current-weather__wind-item">
+            <div class="current-weather__wind-label">Wind</div>
+            <div class="current-weather__wind-speed-container">
+              <div class="current-weather__wind-speed">${Math.round(
+                data.currently.windspeed
+              )} ${data.units.speed}</div>
+              <i
+                aria-label="Wind direction arrow"
+                class="current-weather__arrow-icon fa-solid fa-location-arrow"
+              ></i>
+            </div>
+          </div>
+          <div class="current-weather__humidity-item">
+            <div class="current-weather__humidity-label">Humidity</div>
+            <div class="current-weather__humidity-percentage">${Math.round(
+              data.currently.humidity
+            )}%</div>
+          </div>
+        </div>
+      </div>
+  `;
+
+  element.insertAdjacentHTML('beforeend', html);
 }
 
-async function showWeather(ev) {
-  const data = await handleWeatherEv(ev);
+async function renderCompleteWeather(ev) {
+  const mainEl = document.querySelector('main');
+
+  try {
+    const weatherData = await handleWeatherEv(ev);
+    currentWeatherElement(weatherData, mainEl);
+    displayAlerts(weatherData);
+  } catch (err) {
+    console.error(err);
+  }
 }
+
+getHtmlElement().form.addEventListener('submit', renderCompleteWeather);
+getHtmlElement().btnGeo.addEventListener('click', renderCompleteWeather);
