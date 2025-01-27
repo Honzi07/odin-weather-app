f694d3e6e6afae4a5d64092ef0998890408e8de9
Honzi07 <nyisztor.zsolt@gmail.com>
2025-01-27 06:31:38 +0100

    refactor: move showHttpErrorImg method to error.js module
    - Moved showHttpErrorImg method from data.js to error.js for better modularity.

--- src/js/modules/error.js
+++ src/js/modules/error.js
@@ -1,9 +1,27 @@
 class HttpError extends Error {
-  constructor(message, status) {
+  constructor(message, statusCode) {
     super(message);
     this.name = 'HTTP Error';
-    this.status = status;
+    this.statusCode = statusCode;
+  }
+
+  showHttpErrorImg() {
+    const mainEl = document.querySelector('main');
+    const errWrapperEl = document.createElement('div');
+    const img = document.createElement('img');
+
+    mainEl.innerHTML = '';
+    errWrapperEl.classList.add('http-error-wrapper');
+
+    Object.assign(img, {
+      className: 'http-error-img',
+      src: `https://http.cat/${this.statusCode}`,
+      alt: `HTTP error code with an image of a cat. Code:${this.statusCode}`,
+    });
+
+    mainEl.appendChild(errWrapperEl);
+    errWrapperEl.appendChild(img);
   }
 }
 
-export default HttpError;
+export { HttpError };
